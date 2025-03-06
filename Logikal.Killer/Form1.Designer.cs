cef77caca9670450cb40fe16998ed6fc4248be12
Burak ÇELEBİ <gecesuster@gmail.com>
2025-03-06 21:28:34 +0300

    Proje dosyası ekle.

---
+++ Logikal.Killer/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Logikal.Killer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.radioGroup1 = new DevExpress.XtraEditors.RadioGroup();
+            ((System.ComponentModel.ISupportInitialize)(this.radioGroup1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(230, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Sonlandır";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(372, 11);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Temizle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 52);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(496, 147);
+            this.listBox1.TabIndex = 3;
+            // 
+            // radioGroup1
+            // 
+            this.radioGroup1.EditValue = false;
+            this.radioGroup1.Location = new System.Drawing.Point(12, 12);
+            this.radioGroup1.Name = "radioGroup1";
+            this.radioGroup1.Properties.Columns = 2;
+            this.radioGroup1.Properties.Items.AddRange(new DevExpress.XtraEditors.Controls.RadioGroupItem[] {
+            new DevExpress.XtraEditors.Controls.RadioGroupItem(true, "Tüm Kullanıcılar", true, null, "All"),
+            new DevExpress.XtraEditors.Controls.RadioGroupItem(false, "Aktif Kullanıcı", true, null, "Current")});
+            this.radioGroup1.Size = new System.Drawing.Size(212, 34);
+            this.radioGroup1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 213);
+            this.Controls.Add(this.radioGroup1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Logikal Killer for v.12";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.radioGroup1.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private DevExpress.XtraEditors.RadioGroup radioGroup1;
+    }
+}
+
---
+++ Logikal.Killer/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Logikal.Killer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.WaitCursor;
+            string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\OFCAS";
+
+            // Komut satırında çalıştırılacak komut
+            string command = $"rmdir /s /q \"{folderPath}\"";
+
+            // Komut satırı işlemi başlat
+            ProcessStartInfo processStartInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                Arguments = "/c " + command, // /c parametresi komutun çalıştırılacağını belirtir
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            // İşlemi başlat
+            using (Process process = new Process())
+            {
+                process.StartInfo = processStartInfo;
+                process.Start();
+
+                // İşlem tamamlanana kadar bekleyin
+                process.WaitForExit();
+
+                // Çıktıyı oku
+                string output = process.StandardOutput.ReadToEnd();
+                listBox1.Items.Add(output);
+                listBox1.Items.Add("AppData Temizlendi.");
+                listBox1.Refresh();
+            }
+            this.Cursor = Cursors.Default;
+            listBox1.SelectedIndex = listBox1.Items.Count - 1;
+            listBox1.TopIndex = listBox1.Items.Count - 1;
+        }
+        public string GetProcessUser(Process process)
+        {
+            string query = "Select * From Win32_Process Where ProcessID = " + process.Id;
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+            ManagementObjectCollection processList = searcher.Get();
+
+            foreach (ManagementObject obj in processList)
+            {
+                object[] argList = new object[] { string.Empty, string.Empty };
+                int returnVal = Convert.ToInt32(obj.InvokeMethod("GetOwner", argList));
+                if (returnVal == 0)
+                {
+                    return $"{argList[1]}\\{argList[0]}"; // Domain\Username
+                }
+            }
+
+            return "SYSTEM";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            var select = Boolean.Parse(radioGroup1.EditValue.ToString());
+            if (select)
+            {
+                Process[] processes = Process.GetProcesses();
+
+                // Mevcut kullanıcının tüm çalışan uygulamalarını bul ve sonlandır
+                foreach (Process process in processes)
+                {
+                    try
+                    {
+                        if (process.MainModule != null)
+                        {
+                            string path = process.MainModule.FileName;
+                            FileInfo fileInfo = new FileInfo(path);
+                            FileVersionInfo myFileVersionInfo =
+                            FileVersionInfo.GetVersionInfo(path);
+                            var pn = myFileVersionInfo.ProductName;
+                            if (!string.IsNullOrEmpty(pn))
+                            {
+                                if (pn.Contains("LogiKal") || pn.Contains("ORGADATA"))
+                                {
+                                    string processUser = GetProcessUser(process);
+                                    process.Kill();
+                                    listBox1.Items.Add($"{processUser} Kullanıcısının sonlandırıldı: " + process.ProcessName);
+                                }
+                            }
+
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        //listBox1.Items.Add("Hata: " + ex.Message);
+                    }
+                }
+            }
+            else
+            {
+                Process[] processes = Process.GetProcesses();
+
+                // Mevcut kullanıcının tüm çalışan uygulamalarını bul ve sonlandır
+                foreach (Process process in processes)
+                {
+                    // Sadece mevcut kullanıcının oturumuna ait işlemleri kontrol et
+                    if (process.SessionId == Process.GetCurrentProcess().SessionId)
+                    {
+                        try
+                        {
+                            if (process.MainModule != null)
+                            {
+                                string path = process.MainModule.FileName;
+                                FileInfo fileInfo = new FileInfo(path);
+                                FileVersionInfo myFileVersionInfo =
+                                FileVersionInfo.GetVersionInfo(path);
+                                var pn = myFileVersionInfo.ProductName;
+                                if (!string.IsNullOrEmpty(pn))
+                                {
+                                    if (pn.Contains("LogiKal") || pn.Contains("ORGADATA"))
+                                    {
+                                        string processUser = GetProcessUser(process);
+                                        process.Kill();
+                                        listBox1.Items.Add($"{processUser} Kullanıcısının sonlandırıldı: " + process.ProcessName);
+                                    }
+                                }
+                                
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            //listBox1.Items.Add("Hata: " + ex.Message);
+                        }
+                    }
+                }
+            }
+
+            listBox1.Items.Add("Logikal uygulamları sonlandırıldı.");
+            listBox1.SelectedIndex = listBox1.Items.Count - 1;
+            listBox1.TopIndex = listBox1.Items.Count - 1;
+        }
+    }
+}
